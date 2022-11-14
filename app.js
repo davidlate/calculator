7fd0f6ee4569c715dc20ccf01b352631cd3e557f
David Earley <david.earley2@gmail.com>
2022-11-14 09:13:34 -0500

    Added functionality forappending numbers, clearing numbers w dbl click, and decimal

--- app.js
+++ app.js
@@ -9,17 +9,35 @@ buttons.forEach(btn => btn.addEventListener('click', updatePage))
 lowInputText = '';
 uppInputText = '';
 decimalUsed = false;
+clearCounter = 0;
 
 
 
 function updatePage(e){
-
-    if (getId(e) === null || getId(e) === undefined) return;
+    
     selection = getId(e);
+    if (getId(e) === null || getId(e) === undefined) return;
+    if (selection.id !== 'clear-error') clearCounter = 0;
+    
+    
 
 
     if (selection.classList.contains('number')){
          appendNum(selection);
+    }
+
+
+    else if (selection.id=='clear-error'){
+        if (clearCounter == 0){
+            clearError();
+            clearCounter ++;
+        } 
+        else clearAll();
+    }
+
+
+    else if (selection.id == 'clear-all'){
+        clearAll();
     }
 
     console.log(selection)
@@ -40,21 +58,36 @@ function getId(e){
     return result
 }
 
+
+
+
 function appendNum(selection){
-
         let append = selection.id
-
-        if(append = '.' && decimalUsed == false){
-            decimalUsed = true;
+        if (append == '.') {
+            if (selection.classList.contains('disabled')) append = '';
+            else selection.classList.add('disabled')
         }
+        
 
-        else if (append == '.' && decimalUsed == true){
-            append = '';
-        }
-
-        lowInputText += selection.id;
+        lowInputText += append;
         lowInput.textContent = lowInputText;
 
     }
 
 
+function clearError(){
+    lowInputText = '';
+    lowInput.textContent = lowInputText + '0';
+}
+
+
+function clearAll(){
+    lowInputText = '';
+    lowInput.textContent = lowInputText + '0'
+    uppInputText = '';
+    uppInput.textContent = uppInputText + '0';
+    
+    document.querySelectorAll('.disabled')
+        .forEach(btn => btn.classList.remove('disabled'))
+
+}
